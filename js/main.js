3184722646d40531c6dd4313f30f787ab7b390cc
danaffn <afanasevda26@gmail.com>
2024-11-02 15:21:37 +0500

    adaptive

--- js/main.js
+++ js/main.js
@@ -29,5 +29,30 @@ $(function () {
     $(this).children('.faq__answer').slideToggle()
   })
 
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  }, 0)
+
+  $('.burger, .overlay').on('click', function (e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.overlay').toggleClass('overlay--show')
+    $('.burger').toggleClass('burger--close')
+  })
+  setInterval(() => {
+    if ($('.header__top').hasClass('header__top--open')) {
+      $('.header__nav-link').on('click', function (e) {
+        $('.header__top').removeClass('header__top--open')
+        $('.overlay').removeClass('overlay--show')
+        $('.burger').removeClass('burger--close')
+      })
+    }
+  }, 0);
+
+
 
 });
